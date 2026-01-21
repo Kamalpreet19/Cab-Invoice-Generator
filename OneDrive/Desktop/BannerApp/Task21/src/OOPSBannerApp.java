9bc3cc2921d65fcdd4b3b4b1520abd9482efbc5f
Kamalpreet19 <kk0924405@gmail.com>
2026-01-22 03:27:17 +0530

    Coded OOPS as Banner using print statements

--- OneDrive/Desktop/BannerApp/Task21/src/OOPSBannerApp.java
+++ OneDrive/Desktop/BannerApp/Task21/src/OOPSBannerApp.java
@@ -6,5 +6,16 @@
 public class OOPSBannerApp {
     public static void main(String args[]){
         System.out.println("OOPS");
+
+        //OOPSBannerApp UC2-OOPS Banner Display Application
+
+        System.out.println("*********" + "   " + "*********" + "   " + "*********" + "   " + "*********");
+        System.out.println("*       *" + "   " + "*       *" + "   " + "*       *" + "   " + "*        ");
+        System.out.println("*       *" + "   " + "*       *" + "   " + "*       *" + "   " + "*        ");
+        System.out.println("*       *" + "   " + "*       *" + "   " + "*********" + "   " + "*********");
+        System.out.println("*       *" + "   " + "*       *" + "   " + "*        " + "   " + "        *");
+        System.out.println("*       *" + "   " + "*       *" + "   " + "*        " + "   " + "        *");
+        System.out.println("*********" + "   " + "*********" + "   " + "*        " + "   " + "*********");
+
     }
 }
