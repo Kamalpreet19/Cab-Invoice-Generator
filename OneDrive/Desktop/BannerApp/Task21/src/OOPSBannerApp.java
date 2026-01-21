3d5237591970d8f915a2cf60bc6e59e87ca2199d
Kamalpreet19 <kk0924405@gmail.com>
2026-01-22 04:37:28 +0530

    Coded OOPS as banner logic using functions

--- OneDrive/Desktop/BannerApp/Task21/src/OOPSBannerApp.java
+++ OneDrive/Desktop/BannerApp/Task21/src/OOPSBannerApp.java
@@ -4,8 +4,16 @@
  */
 
 public class OOPSBannerApp {
-    public static void main(String args[]) {
+    //public static void main(String args[]) {
+        /*
         System.out.println("OOPS");
+
+
+
+
+
+
+
 
         //OOPSBannerApp UC2-OOPS Banner Display Application
 
@@ -18,9 +26,16 @@ public class OOPSBannerApp {
         System.out.println("*********" + "   " + "*********" + "   " + "*        " + "   " + "*********");
 
 
-        System.out.println();
+        System.out.println();*/
+
+
+
+
+
+
 
         //OOPSBannerApp UC3-OOPS Banner Display (used String.join())
+        /*
         System.out.println(String.join("   ",
                 "*********", "*********", "*********", "*********"));
 
@@ -43,10 +58,16 @@ public class OOPSBannerApp {
                 "*********", "*********", "*        ", "*********"));
 
 
-        System.out.println();
+        System.out.println();*/
+
+
+
+
+
+
 
     //OOPSBannerApp UC4-OOPS Banner print using String.join() and a loop
-
+     /*
     String[] banner = {
             String.join("   ",
                     "*********", "*********", "*********", "*********"),
@@ -76,9 +97,20 @@ public class OOPSBannerApp {
     }
 
 
-        System.out.println();
+        System.out.println();*/
+
+
+
+
+
+
+
+
+
+
 
         //OOPSBannerApp UC5- Prints OOPS banner using inline array and loop.
+        /*
         String[] banner2 = {
                 String.join("   ", "*********", "*********", "*********", "*********"),
                 String.join("   ", "*       *", "*       *", "*       *", "*        "),
@@ -89,10 +121,75 @@ public class OOPSBannerApp {
                 String.join("   ", "*********", "*********", "*        ", "*********")
         };
 
-        // Print banner using for-each loop
+
         for (String line : banner2) {
             System.out.println(line);
         }
+        System.out.println();*/
+
+
+
+
+
+
+        //OOPSBannerApp UC6- Refactor Banner Logic into Functions
+        public static String[] getOPattern() {
+            return new String[]{
+                    "    ***    ",
+                    " **     ** ",
+                    "**       **",
+                    "**       **",
+                    "**       **",
+                    " **     ** ",
+                    "    ***    "
+            };
+
+            }
+
+
+    public static String[] getPPattern() {
+        return new String[]{
+                "*********",
+                "*       *",
+                "*       *",
+                "*********",
+                "*        ",
+                "*        ",
+                "*        "
+
+        };
+
+    }
+
+    public static String[] getSPattern() {
+        return new String[]{
+                "*********",
+                "*        ",
+                "*        ",
+                "*********",
+                "        *",
+                "        *",
+                "*********",
+
+        };
+
+    }
+
+    public static void main(String args[]){
+
+            String[] oPattern=getOPattern();
+            String[] pPattern=getPPattern();
+            String[] sPattern=getSPattern();
+
+
+
+
+            for(int i=0;i<oPattern.length;i++){
+                System.out.println(oPattern[i] + " " + oPattern[i] + " " + pPattern[i] + " " + sPattern[i]);
+
+            }
+
+
     }
 
 }
